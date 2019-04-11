463bfa36f66568c486f73ed5337aa74be84dde66
mtangaro <ma.tangaro@gmail.com>
2019-04-11 13:54:34 +0000

    fix permissions for one command mode

--- galaxy/recover.sh
+++ galaxy/recover.sh
@@ -2,8 +2,8 @@
 LOGFILE="/tmp/recover.log"
 
 # Restart cvmfs
-systemctl restart autofs > $LOGFILE 2>&1 && /usr/bin/cvmfs_config killall > $LOGFILE 2>&1 && /usr/bin/cvmfs_config probe
+sudo systemctl restart autofs > $LOGFILE 2>&1 && sudo /usr/bin/cvmfs_config killall > $LOGFILE 2>&1 && sudo /usr/bin/cvmfs_config probe
 
 # Restart galaxy
-/usr/local/bin/galaxy-startup > $LOGFILE 2>&1
+sudo /usr/local/bin/galaxy-startup > $LOGFILE 2>&1
 /home/galaxy/.laniakea_utils/check_instance
