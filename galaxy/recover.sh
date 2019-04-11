f9c3dbbb28f8d43b579a7169e8133589ea642e44
mtangaro <ma.tangaro@gmail.com>
2019-04-11 13:42:15 +0000

    add restart galaxy script

---
+++ galaxy/recover.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+LOGFILE="/tmp/recover.log"
+
+# Restart cvmfs
+systemctl restart autofs > $LOGFILE 2>&1 && /usr/bin/cvmfs_config killall > $LOGFILE 2>&1 && /usr/bin/cvmfs_config probe
+
+# Restart galaxy
+/usr/local/bin/galaxy-startup > $LOGFILE 2>&1
+/home/galaxy/.laniakea_utils/check_instance
