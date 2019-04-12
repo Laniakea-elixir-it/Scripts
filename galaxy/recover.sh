c7f83e4e301c6bda00ab80f25a0203744f287ac7
mtangaro <ma.tangaro@gmail.com>
2019-04-12 08:08:59 +0000

    remove sudo

--- galaxy/recover.sh
+++ galaxy/recover.sh
@@ -2,8 +2,8 @@
 LOGFILE="/tmp/recover$(date +"-%b-%d-%y-%H%M%S").log"
 
 # Restart cvmfs
-sudo systemctl restart autofs > $LOGFILE 2>&1 && sudo /usr/bin/cvmfs_config killall > $LOGFILE 2>&1 && sudo /usr/bin/cvmfs_config probe
+systemctl restart autofs > $LOGFILE 2>&1 && /usr/bin/cvmfs_config killall > $LOGFILE 2>&1 && /usr/bin/cvmfs_config probe
 
 # Restart galaxy
-sudo /usr/local/bin/galaxy-startup > $LOGFILE 2>&1
+/usr/local/bin/galaxy-startup > $LOGFILE 2>&1
 /home/galaxy/.laniakea_utils/check_instance
